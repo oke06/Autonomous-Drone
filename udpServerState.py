0c317e82ab65fbb8222cc58a0dd06ae1c9eca5dd
oke06 <okatakyieba09@gmail.com>
2026-02-09 13:34:00 +0000

    Push the project files to github

--- udpServerState.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server.bind("0.0.0.0", 8890)
-
-while True:
-    message, address = server.recvfrom(1024)
-    print(message.decode("utf-8"))
